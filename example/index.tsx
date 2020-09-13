1d057eba95ec19f6daed39e82cabeca79324b1b8
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-09-12 17:37:34 -0700

    add object of cart options

--- example/index.tsx
+++ example/index.tsx
@@ -19,11 +19,27 @@ import Product from './components/Product'
 
 const cn = (arr: (string | false | null)[]) => arr.filter(a => a).join(' ')
 
+const stores = {
+  airsign: {
+    domain: 'airsign-co.myshopify.com',
+    storefrontAccessToken: '84aaa88f572f6ffabb3c83b0bfbc7365',
+  },
+  highTide: {
+    domain: 'hightidenyc.myshopify.com',
+    storefrontAccessToken: '9395d0399ded34d4a90d8e45db500a76',
+  },
+  pistils: {
+    domain: 'pistils-nursery.myshopify.com',
+    storefrontAccessToken: 'ffb71a0587c96b47c38e04c33d5b5dd2',
+  },
+  testStore: {
+    storefrontAccessToken: '7b6d57462efa8482c96361a9e9f9045d',
+    domain: '202003-tm.myshopify.com',
+  },
+}
+
 // Remember to clear cookies when switching values
-export const client = Client.buildClient({
-  domain: 'pistils-nursery.myshopify.com',
-  storefrontAccessToken: 'ffb71a0587c96b47c38e04c33d5b5dd2',
-})
+export const client = Client.buildClient(stores.airsign)
 
 const App = () => {
   return (
